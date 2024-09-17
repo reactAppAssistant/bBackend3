20143b5f477fbe7100ab49e86441362a6bfa047c
reactAppAssistant <82675519+reactAppAssistant@users.noreply.github.com>
2024-09-17 10:18:57 +0530

    ppost

--- models/Post.js
+++ models/Post.js
@@ -4,6 +4,7 @@ const postSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   username: { type: String, required: true },
   email: { type: String, required: true },
+  imageUrl: { type: String },  // Add imageUrl field
   content: { type: String, required: true },
   reactions: { type: Map, of: Number, default: {} }, // Map to store reactions and their counts
   timestamp: { type: Date, default: Date.now },
