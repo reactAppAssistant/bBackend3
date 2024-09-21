28991298fee2dfcbb8d2e39c727fde26470bbccb
reactAppAssistant <82675519+reactAppAssistant@users.noreply.github.com>
2024-09-21 09:53:38 +0530

    Posts with profile Image

--- models/Post.js
+++ models/Post.js
@@ -4,6 +4,7 @@ const postSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   username: { type: String, required: true },
   email: { type: String, required: true },
+  profileImageUrl: {type: String, required: true},
   imageUrl: { type: String, default: "" },  // Add imageUrl field
   content: { type: String, default:""},
   reactions: { type: Map, of: Number, default: {} }, // Map to store reactions and their counts
