902539dd2598e393ebd57c17107c139a9c223649
reactAppAssistant <82675519+reactAppAssistant@users.noreply.github.com>
2024-09-25 11:34:04 +0530

    Add files via upload

---
+++ models/News.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const newsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const News = mongoose.model('News', newsSchema);
+
+module.exports = News;
